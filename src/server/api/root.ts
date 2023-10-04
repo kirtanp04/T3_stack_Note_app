210aca2dc7d32b145948c6a2e0de2c36eb297acf
kirtan <kirtanpatel6189@gmail.com>
2023-10-04 11:15:30 +0530

    updated

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,11 +3,7 @@ import { exampleRouter } from "./routers/example";
 import { topicRouter } from "./routers/topic";
 import { noteRouter } from "./routers/note";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here
- */
+
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topic: topicRouter,
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,4 @@
-/**
- * This is the client-side entrypoint for your tRPC API.
- * It is used to create the `api` object which contains the Next.js
- * App-wrapper, as well as your type-safe React Query hooks.
- *
- * We also create a few inference helpers for input and output types
- */
+
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
@@ -18,22 +12,11 @@ const getBaseUrl = () => {
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
-/** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
     return {
-      /**
-       * Transformer used for data de-serialization from the server.
-       *
-       * @see https://trpc.io/docs/data-transformers
-       **/
+      
       transformer: superjson,
-
-      /**
-       * Links used to determine request flow from client to server.
-       *
-       * @see https://trpc.io/docs/links
-       * */
       links: [
         loggerLink({
           enabled: (opts) =>
@@ -46,24 +29,9 @@ export const api = createTRPCNext<AppRouter>({
       ],
     };
   },
-  /**
-   * Whether tRPC should await queries when server rendering pages.
-   *
-   * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
-   */
   ssr: false,
 });
 
-/**
- * Inference helper for inputs.
- *
- * @example type HelloInput = RouterInputs['example']['hello']
- **/
 export type RouterInputs = inferRouterInputs<AppRouter>;
 
-/**
- * Inference helper for outputs.
- *
- * @example type HelloOutput = RouterOutputs['example']['hello']
- **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
